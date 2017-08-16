2c42ffcd0d3b42a8c9a86f4bbf9b30de9a8e9d16
jc463705 <thomas.napier@my.jcu.edu.au>
2017-08-16 13:45:02 +1000

    Add suitable docstrings and fix formatting issues

--- prac_03/ascii_table_v2.py
+++ prac_03/ascii_table_v2.py
@@ -1,19 +1,30 @@
+"""
+CP1404 Practical
+ASCII table and converter with functions and docstrings
+"""
+
 LOWER = 33
 UPPER = 127
 
+
 def main():
+    """Get and convert ASCII values and characters"""
     character = input("Enter a character: ")
     print("The ASCII code for {} is {}".format(character, ord(character)))
     number = get_number(LOWER, UPPER)
     print("The character for {} is {}".format(number, chr(number)))
 
+    # print the ascii table with no columns
     for value in range(LOWER, UPPER + 1):
         print("{:3} {:>4}".format(value, chr(value)))
 
+
 def get_number(lower=LOWER, upper=UPPER):
+    """Get a number from the user between the set boundries"""
     number = int(input("Enter a number between {} and {}: ".format(lower, upper)))
     while number < lower or number > upper:
         number = int(input("Enter a number between {} and {}:".format(lower, upper)))
     return number
+
 
 main()
