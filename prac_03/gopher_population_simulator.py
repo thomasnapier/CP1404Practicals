4c06652e70e23dc4f4dfa8646dcc66cc961a9fc3
jc463705 <thomas.napier@my.jcu.edu.au>
2017-08-16 14:49:18 +1000

    Update gopher program

--- prac_03/gopher_population_simulator.py
+++ prac_03/gopher_population_simulator.py
@@ -10,10 +10,10 @@ import random
 def main():
     """Population calculation program"""
     population = 1000  # initial population
-    birth_lower = 0.10  # 10%
-    birth_upper = 0.20  # 20%
-    death_lower = 0.05  # 5%
-    death_upper = 0.25  # 25%
+    birth_lower = 100  # 10%
+    birth_upper = 200  # 20%
+    death_lower = 50  # 5%
+    death_upper = 250  # 25%
     year = 10  # number of years
     year_count = 0
     dead = 0
@@ -22,16 +22,26 @@ def main():
     print("Starting population: {}".format(population))
     for number in range(year):
         dead = calculate_deaths(population, death_lower, death_upper)
-        born = calculate_births(population, dead, birth_lower, birth_upper)
+        born = calculate_births(population, birth_lower, birth_upper)
         year_count += 1
         print("Year {}".format(year))
         print("*****")
-        print("{} gophers were born")
+        print("{} gophers were born. {} died".format(born, dead))
+        print("Population: {}".format(population))
 
 
-def calculate_deaths(lower, upper):
+def calculate_deaths(population, lower, upper):
     """calculate the amount of deaths based on a random value"""
-    percent_dead = random.randint(lower, upper)
+    percent_dead = random.randrange(lower, upper)
+    dead = population - percent_dead
+    return dead
+
+
+def calculate_births(population, lower, upper):
+    """calculate the amount of births based on a random value"""
+    percent_born = random.randrange(lower, upper)
+    born = population + percent_born
+    return born
 
 
 main()
