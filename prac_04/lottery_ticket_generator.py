d38194511dda51258ee4bf63f8c415dbf893f215
jc463705 <thomas.napier@my.jcu.edu.au>
2017-08-23 14:50:12 +1000

    Update income and ticket program

--- prac_04/lottery_ticket_generator.py
+++ prac_04/lottery_ticket_generator.py
@@ -1,8 +1,8 @@
 import random
 
 NUMBERS_PER_LINE = 6
-MINIMUM = 1
-MAXIMUM = 45
+MIN = 1
+MAX = 45
 
 
 def main():
@@ -10,10 +10,9 @@ def main():
     while number_of_picks <= 0:
         print("Invalid number of picks")
         number_of_picks = int(input("How many quick picks? "))
-
     for number in range(number_of_picks):
         for line in range(NUMBERS_PER_LINE):
-            number = random.randint(MINIMUM, MAXIMUM)
+            number = random.randint(MIN, MAX)
             print("{:3}".format(number), end="")
         print()
 
