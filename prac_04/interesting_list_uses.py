d45e26873522d4177cd379ce64d7d10c6ae7a91b
jc463705 <thomas.napier@my.jcu.edu.au>
2017-08-23 13:47:25 +1000

    Add extended exercise to interesting list uses program

--- prac_04/interesting_list_uses.py
+++ prac_04/interesting_list_uses.py
@@ -1,10 +1,13 @@
 def main():
     numbers = []
-    for number in range(1, 6):
-        num = int(input("Number: "))
-        numbers.append(num)
+    number_count = 1
+    number = 0
+    while number >= 0:
+        number = int(input("Number {}: ".format(number_count)))
+        numbers.append(number)
+        number_count += 1
     print("The first number is {}".format(numbers[0]))
-    print("The second number is {}".format(numbers[4]))
+    print("The last number is {}".format(numbers[-1]))
     print("The smallest number is {}".format(min(numbers)))
     print("The largest number is {}".format(max(numbers)))
     average = sum(numbers) / len(numbers)
--- prac_04/lottery_ticket_generator.py
+++ prac_04/lottery_ticket_generator.py
@@ -4,6 +4,7 @@ NUMBERS_PER_LINE = 6
 MINIMUM = 1
 MAXIMUM = 45
 
+
 def main():
     number_of_picks = int(input("How many quick picks? "))
     while number_of_picks <= 0:
@@ -13,7 +14,8 @@ def main():
     for number in range(number_of_picks):
         for line in range(NUMBERS_PER_LINE):
             number = random.randint(MINIMUM, MAXIMUM)
-            print("{:3}".format(number), end = "")
+            print("{:3}".format(number), end="")
         print()
+
 
 main()
