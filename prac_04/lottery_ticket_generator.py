f3f2c990192bff679a24d4a3ec24120282ef431e
thomasnapier <thomas.napier@my.jcu.edu.au>
2017-08-30 09:26:25 +1000

    Add additional functionality to quick picks program

--- prac_04/lottery_ticket_generator.py
+++ prac_04/lottery_ticket_generator.py
@@ -4,16 +4,21 @@ NUMBERS_PER_LINE = 6
 MINIMUM = 1
 MAXIMUM = 45
 
+
 def main():
+    quick_pick = []
     number_of_picks = int(input("How many quick picks? "))
     while number_of_picks <= 0:
         print("Invalid number of picks")
         number_of_picks = int(input("How many quick picks? "))
-
     for number in range(number_of_picks):
         for line in range(NUMBERS_PER_LINE):
             number = random.randint(MINIMUM, MAXIMUM)
-            print("{:3}".format(number), end = "")
-        print()
+            while number in quick_pick:  # while the generated number is already in the quick picks list
+                number = random.randint(MINIMUM, MAXIMUM)  # generate another
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
 
 main()
