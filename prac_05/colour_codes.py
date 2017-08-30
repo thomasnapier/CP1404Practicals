092826e07999f8d5d29a37839262ab29852457bb
jc463705 <thomas.napier@my.jcu.edu.au>
2017-08-30 14:43:33 +1000

    Update programs with feedback from JP

--- prac_05/colour_codes.py
+++ prac_05/colour_codes.py
@@ -1,13 +1,10 @@
 COLOUR_CODES = {"Aliceblue": "#f0f8ff", "Blanchedalmond": "#ffebcd", "Blueviolet": "#8a2be2", "Cadetblue": "#5f9ea0",
                 "Blue": "#0000ff", "Cyan": "00ffff", "Darkgreen": "#006400", "Dodgerblue": "#1e90ff"}
 
-colour = input("Enter colour name: ").capitalize()
+colour = input("Enter colour name: ")
 while colour != "":
     if colour in COLOUR_CODES:
-        print(colour, "is", COLOUR_CODES[colour])
+        print("{} is {}".format(colour, COLOUR_CODES[colour]))
     else:
         print("Invalid colour name")
-    colour = input("Enter colour name: ").capitalize()
-
-
-
+    colour = input("Enter colour name: ")
